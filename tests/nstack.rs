f61608afada07e99b154aca354e75ec56adfc358
Eduardo Leegwater Simões <eduardo@dusk.network>
2021-11-04 11:37:11 +0100

    Rkyv Integration (#40)
    Update to the new  `rkyv`-enabled version of `microkelvin`. 
    
    Resolves: #37
    
    Co-authored-by: Kristoffer Ström <kristoffer@dusk.network>

--- tests/nstack.rs
+++ tests/nstack.rs
@@ -4,169 +4,121 @@
 //
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
-use canonical::{Canon, CanonError};
-use canonical_derive::Canon;
-
+use microkelvin::*;
 use nstack::NStack;
 
-use core::borrow::Borrow;
-use microkelvin::*;
-
 #[test]
-fn trivial() -> Result<(), CanonError> {
+fn trivial() {
     let mut nt = NStack::<u32, Cardinality>::new();
-    assert_eq!(nt.pop()?, None);
-    Ok(())
+    assert_eq!(nt.pop(), None);
 }
 
 #[test]
-fn push_pop() -> Result<(), CanonError> {
+fn push_pop() {
     let mut nt = NStack::<_, Cardinality>::new();
-    nt.push(8)?;
-    assert_eq!(nt.pop()?, Some(8));
-    Ok(())
+    nt.push(8);
+    assert_eq!(nt.pop(), Some(8));
 }
 
 #[test]
-fn double() -> Result<(), CanonError> {
+fn double() {
     let mut nt = NStack::<_, Cardinality>::new();
-    nt.push(0)?;
-    nt.push(1)?;
-    assert_eq!(nt.pop()?, Some(1));
-    assert_eq!(nt.pop()?, Some(0));
-    Ok(())
+    nt.push(0);
+    nt.push(1);
+    assert_eq!(nt.pop(), Some(1));
+    assert_eq!(nt.pop(), Some(0));
 }
 
 #[test]
-fn multiple() -> Result<(), CanonError> {
+fn multiple() {
     let n = 1024;
 
     let mut nt = NStack::<_, Cardinality>::new();
 
     for i in 0..n {
-        nt.push(i)?;
+        nt.push(i);
     }
 
     for i in 0..n {
-        assert_eq!(nt.pop()?, Some(n - i - 1));
+        assert_eq!(nt.pop(), Some(n - i - 1));
     }
 
-    assert_eq!(nt.pop()?, None);
-    Ok(())
+    assert_eq!(nt.pop(), None);
 }
 
 #[test]
-fn nth() -> Result<(), CanonError> {
+fn nth() {
     let n: u64 = 1024;
 
     let mut nstack = NStack::<_, Cardinality>::new();
 
     for i in 0..n {
-        nstack.push(i)?;
+        nstack.push(i);
     }
 
     for i in 0..n {
-        assert_eq!(*nstack.nth(i)?.expect("Some(_)"), i);
+        assert_eq!(nstack.walk(Nth(i)).expect("Some(_)").leaf(), i);
     }
 
-    assert!(nstack.nth(n)?.is_none());
-    Ok(())
+    assert!(nstack.walk(Nth(n)).is_none());
 }
 
 #[test]
-fn nth_mut() -> Result<(), CanonError> {
+fn nth_mut() {
     let n: u64 = 1024;
 
     let mut nstack = NStack::<_, Cardinality>::new();
 
     for i in 0..n {
-        nstack.push(i)?;
+        nstack.push(i);
     }
 
     for i in 0..n {
-        *nstack.nth_mut(i)?.expect("Some(_)") += 1;
+        *nstack.walk_mut(Nth(i)).expect("Some(_)") += 1;
     }
 
     for i in 0..n {
-        assert_eq!(*nstack.nth(i)?.expect("Some(_)"), i + 1);
+        assert_eq!(nstack.walk(Nth(i)).expect("Some(_)").leaf(), i + 1);
     }
-
-    Ok(())
 }
 
 // Assert that all branches are always of the same length
 #[test]
-fn branch_lengths() -> Result<(), CanonError> {
+fn branch_lengths() {
     let n = 256;
 
     let mut nt = NStack::<_, Cardinality>::new();
 
     for i in 0..n {
-        nt.push(i)?;
+        nt.push(i);
     }
 
-    let length_zero = nt.nth(0)?.expect("Some(_)").depth();
+    let length_reference = nt.walk(First).expect("Some(_)").depth();
 
-    for i in 1..n {
-        assert_eq!(length_zero, nt.nth(i)?.expect("Some(_)").depth())
-    }
-
-    Ok(())
-}
-
-#[derive(Canon, Clone, Debug)]
-struct MaxAndCardinality<K> {
-    cardinality: Cardinality,
-    max: MaxKey<K>,
-}
-
-impl<K> Default for MaxAndCardinality<K> {
-    fn default() -> Self {
-        Self {
-            max: Default::default(),
-            cardinality: Default::default(),
-        }
+    for i in 0..n {
+        assert_eq!(length_reference, nt.walk(Nth(i)).expect("Some(_)").depth())
     }
 }
 
-impl<K> Borrow<Cardinality> for MaxAndCardinality<K> {
-    fn borrow(&self) -> &Cardinality {
-        &self.cardinality
-    }
-}
+#[test]
+fn persist() {
+    let n: u64 = 1024;
 
-impl<K> Borrow<MaxKey<K>> for MaxAndCardinality<K> {
-    fn borrow(&self) -> &MaxKey<K> {
-        &self.max
+    let mut stack = NStack::<_, Cardinality>::new();
+    for i in 0..n {
+        stack.push(i);
     }
-}
 
-impl<K, L> Annotation<L> for MaxAndCardinality<K>
-where
-    L: Keyed<K>,
-    K: Clone + Ord + Canon,
-{
-    fn from_leaf(leaf: &L) -> Self {
-        Self {
-            cardinality: Cardinality::from_leaf(leaf),
-            max: MaxKey::from_leaf(leaf),
-        }
+    let stored = Portal::put(&stack);
+    let restored = Portal::get(stored);
+
+    // empty original
+    for i in 0..n {
+        assert_eq!(stack.pop().unwrap(), n - i - 1);
     }
-}
 
-impl<A, K> Combine<A> for MaxAndCardinality<K>
-where
-    A: Borrow<Cardinality> + Borrow<MaxKey<K>>,
-    K: Clone + Ord,
-{
-    fn combine<C>(anno: AnnoIter<C, A>) -> Self
-    where
-        C: Compound<A>,
-        A: Annotation<C::Leaf>,
-    {
-        Self {
-            cardinality: Cardinality::combine(anno.clone()),
-            max: MaxKey::combine(anno),
-        }
+    // check restored copy
+    for i in 0..n {
+        assert_eq!(restored.walk(Nth(i)).unwrap().leaf(), i);
     }
 }
