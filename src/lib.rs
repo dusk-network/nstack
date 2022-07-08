6b783f2bc924bd57bd41518f64aa5e6e1394eafc
Eduardo Leegwater Simões <eduardo@dusk.network>
2022-07-08 12:20:31 +0200

    Remove `rkyv` and `microkelvin` dependencies

--- src/lib.rs
+++ src/lib.rs
@@ -8,156 +8,34 @@
 //!
 //! A stack data structure with indexed lookup.
 #![no_std]
-#![allow(clippy::large_enum_variant)]
-#![allow(clippy::type_complexity)]
 
-use core::borrow::BorrowMut;
+extern crate alloc;
+
+use alloc::rc::Rc;
 use core::mem;
-
-use bytecheck::CheckBytes;
-use microkelvin::{
-    Annotation, ArchivedChild, ArchivedCompound, Child, ChildMut, Compound,
-    Link, MutableLeaves, StoreProvider, StoreRef, StoreSerializer,
-};
-use rkyv::validation::validators::DefaultValidator;
-use rkyv::{option::ArchivedOption, Archive, Deserialize, Serialize};
 
 const N: usize = 4;
 
-// Clippy complains about the difference in size between the enum variants, however since the
-// most common case is the larger enum, and node traversal should be fast, we trade memory for
-// speed here.
-#[derive(Clone, Archive, Serialize, Deserialize)]
-#[archive_attr(derive(CheckBytes))]
-#[archive(bound(serialize = "
-  T: Archive + Serialize<StoreSerializer<I>>,
-  A: Clone + Annotation<T>,
-  I: Clone,
-  __S: Sized + BorrowMut<StoreSerializer<I>>,
-"))]
-#[archive(bound(deserialize = "
-  NStack<T, A, I>: Clone,
-  A: Clone, // + Annotation<NStack<T, A, I>>,
-  I: Clone,
-  __D: StoreProvider<I>,
-"))]
-pub enum NStack<T, A, I> {
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum NStack<T> {
     Leaf([Option<T>; N]),
-    Node(#[omit_bounds] [Option<Link<NStack<T, A, I>, A, I>>; N]),
+    Node([Option<Rc<NStack<T>>>; N]),
 }
 
-impl<T, A, I> Compound<A, I> for NStack<T, A, I>
-where
-    T: Archive,
-    A: Annotation<T>,
-{
-    type Leaf = T;
-
-    fn child(&self, ofs: usize) -> Child<Self, A, I> {
-        match (ofs, self) {
-            (0, NStack::Node([Some(a), _, _, _])) => Child::Link(a),
-            (1, NStack::Node([_, Some(b), _, _])) => Child::Link(b),
-            (2, NStack::Node([_, _, Some(c), _])) => Child::Link(c),
-            (3, NStack::Node([_, _, _, Some(d)])) => Child::Link(d),
-            (0, NStack::Leaf([Some(a), _, _, _])) => Child::Leaf(a),
-            (1, NStack::Leaf([_, Some(b), _, _])) => Child::Leaf(b),
-            (2, NStack::Leaf([_, _, Some(c), _])) => Child::Leaf(c),
-            (3, NStack::Leaf([_, _, _, Some(d)])) => Child::Leaf(d),
-            _ => Child::End,
-        }
-    }
-
-    fn child_mut(&mut self, ofs: usize) -> ChildMut<Self, A, I> {
-        match (ofs, self) {
-            (0, NStack::Node([Some(a), _, _, _])) => ChildMut::Link(a),
-            (1, NStack::Node([_, Some(b), _, _])) => ChildMut::Link(b),
-            (2, NStack::Node([_, _, Some(c), _])) => ChildMut::Link(c),
-            (3, NStack::Node([_, _, _, Some(d)])) => ChildMut::Link(d),
-            (0, NStack::Leaf([Some(a), _, _, _])) => ChildMut::Leaf(a),
-            (1, NStack::Leaf([_, Some(b), _, _])) => ChildMut::Leaf(b),
-            (2, NStack::Leaf([_, _, Some(c), _])) => ChildMut::Leaf(c),
-            (3, NStack::Leaf([_, _, _, Some(d)])) => ChildMut::Leaf(d),
-            _ => ChildMut::End,
-        }
-    }
-}
-
-impl<T, A, I> ArchivedCompound<NStack<T, A, I>, A, I>
-    for ArchivedNStack<T, A, I>
-where
-    T: Archive,
-    A: Annotation<T>,
-{
-    fn child(&self, ofs: usize) -> ArchivedChild<NStack<T, A, I>, A, I> {
-        match (ofs, self) {
-            (0, ArchivedNStack::Node([ArchivedOption::Some(a), _, _, _])) => {
-                ArchivedChild::Link(a)
-            }
-            (1, ArchivedNStack::Node([_, ArchivedOption::Some(b), _, _])) => {
-                ArchivedChild::Link(b)
-            }
-            (2, ArchivedNStack::Node([_, _, ArchivedOption::Some(c), _])) => {
-                ArchivedChild::Link(c)
-            }
-            (3, ArchivedNStack::Node([_, _, _, ArchivedOption::Some(d)])) => {
-                ArchivedChild::Link(d)
-            }
-            (0, ArchivedNStack::Leaf([ArchivedOption::Some(a), _, _, _])) => {
-                ArchivedChild::Leaf(a)
-            }
-            (1, ArchivedNStack::Leaf([_, ArchivedOption::Some(b), _, _])) => {
-                ArchivedChild::Leaf(b)
-            }
-            (2, ArchivedNStack::Leaf([_, _, ArchivedOption::Some(c), _])) => {
-                ArchivedChild::Leaf(c)
-            }
-            (3, ArchivedNStack::Leaf([_, _, _, ArchivedOption::Some(d)])) => {
-                ArchivedChild::Leaf(d)
-            }
-            _ => ArchivedChild::End,
-        }
-    }
-}
-
-impl<T, A, I> MutableLeaves for NStack<T, A, I> where A: Annotation<T> {}
-
-impl<T, A, I> Default for NStack<T, A, I>
-where
-    A: Annotation<T>,
-{
+impl<T> Default for NStack<T> {
     fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl<T> NStack<T> {
+    /// Creates a new empty NStack
+    pub const fn new() -> Self {
         NStack::Leaf([None, None, None, None])
     }
 }
 
-enum Push<T> {
-    Ok,
-    NoRoom { t: T, depth: usize },
-}
-
-enum Pop<T> {
-    Ok(T),
-    Last(T),
-    None,
-}
-
-impl<T, A, I> NStack<T, A, I>
-where
-    Self: Archive,
-    <NStack<T, A, I> as Archive>::Archived: ArchivedCompound<Self, A, I>
-        + Deserialize<Self, StoreRef<I>>
-        + for<'a> CheckBytes<DefaultValidator<'a>>,
-    T: Archive + Clone + for<'a> CheckBytes<DefaultValidator<'a>>,
-    A: Annotation<<Self as Compound<A, I>>::Leaf>
-        + Annotation<T>
-        + Annotation<NStack<T, A, I>>,
-    I: Clone + for<'any> CheckBytes<DefaultValidator<'any>>,
-{
-    /// Creates a new empty NStack
-    pub fn new() -> Self {
-        Self::default()
-    }
-
+impl<T: Clone> NStack<T> {
     /// Pushes a new element onto the stack
     pub fn push(&mut self, t: T) {
         match self._push(t) {
@@ -166,7 +44,7 @@ where
                 let old_root = mem::take(self);
 
                 let mut new_node = [None, None, None, None];
-                new_node[0] = Some(Link::new(old_root));
+                new_node[0] = Some(Rc::new(old_root));
 
                 *self = NStack::Node(new_node);
 
@@ -200,7 +78,7 @@ where
                     match &mut node[i] {
                         None => (),
                         Some(annotated) => {
-                            match annotated.inner_mut()._push(t) {
+                            match Rc::make_mut(annotated)._push(t) {
                                 Push::Ok => return Push::Ok,
                                 Push::NoRoom { t, depth } => {
                                     // Are we in the last node
@@ -225,7 +103,7 @@ where
                                                 NStack::new(),
                                             );
                                             new_node = NStack::Node([
-                                                Some(Link::new(old_root)),
+                                                Some(Rc::new(old_root)),
                                                 None,
                                                 None,
                                                 None,
@@ -243,7 +121,7 @@ where
                 }
                 // break out and insert
                 if let Some((new_node, index)) = insert_node {
-                    node[index] = Some(Link::new(new_node));
+                    node[index] = Some(Rc::new(new_node));
                 } else {
                     unreachable!()
                 }
@@ -286,7 +164,7 @@ where
                     // reverse
                     let i = N - i - 1;
                     if let Some(ref mut subtree) = node[i] {
-                        match subtree.inner_mut()._pop() {
+                        match Rc::make_mut(subtree)._pop() {
                             Pop::Ok(t) => return Pop::Ok(t),
                             Pop::Last(t) => {
                                 if i == 0 {
@@ -309,4 +187,15 @@ where
             }
         }
     }
+}
+
+enum Push<T> {
+    Ok,
+    NoRoom { t: T, depth: usize },
+}
+
+enum Pop<T> {
+    Ok(T),
+    Last(T),
+    None,
 }
--- tests/nstack.rs
+++ tests/nstack.rs
@@ -4,26 +4,24 @@
 //
 // Copyright (c) DUSK NETWORK. All rights reserved.
 
-use microkelvin::*;
 use nstack::NStack;
-use rkyv::{Archive, Deserialize};
 
 #[test]
 fn trivial() {
-    let mut nt = NStack::<u32, Cardinality, OffsetLen>::new();
+    let mut nt = NStack::<u32>::new();
     assert_eq!(nt.pop(), None);
 }
 
 #[test]
 fn push_pop() {
-    let mut nt = NStack::<_, Cardinality, OffsetLen>::new();
+    let mut nt = NStack::new();
     nt.push(8);
     assert_eq!(nt.pop(), Some(8));
 }
 
 #[test]
 fn double() {
-    let mut nt = NStack::<_, Cardinality, OffsetLen>::new();
+    let mut nt = NStack::new();
     nt.push(0);
     nt.push(1);
     assert_eq!(nt.pop(), Some(1));
@@ -34,7 +32,7 @@ fn double() {
 fn multiple() {
     let n = 1024;
 
-    let mut nt = NStack::<_, Cardinality, OffsetLen>::new();
+    let mut nt = NStack::new();
 
     for i in 0..n {
         nt.push(i);
@@ -45,87 +43,4 @@ fn multiple() {
     }
 
     assert_eq!(nt.pop(), None);
-}
-
-#[test]
-fn nth() {
-    let n: u64 = 1024;
-
-    let mut nstack = NStack::<_, Cardinality, OffsetLen>::new();
-
-    for i in 0..n {
-        nstack.push(i);
-    }
-
-    for i in 0..n {
-        assert_eq!(nstack.walk(Nth(i)).expect("Some(_)").leaf(), i);
-    }
-
-    assert!(nstack.walk(Nth(n)).is_none());
-}
-
-#[test]
-fn nth_mut() {
-    let n: u64 = 1024;
-
-    let mut nstack = NStack::<_, Cardinality, OffsetLen>::new();
-
-    for i in 0..n {
-        nstack.push(i);
-    }
-
-    for i in 0..n {
-        let mut branch_mut = nstack.walk_mut(Nth(i)).expect("Some(_)");
-        *branch_mut.leaf_mut() += 1;
-    }
-
-    for i in 0..n {
-        assert_eq!(nstack.walk(Nth(i)).expect("Some(_)").leaf(), i + 1);
-    }
-}
-
-// Assert that all branches are always of the same length
-#[test]
-fn branch_lengths() {
-    let n = 256;
-
-    let mut nt = NStack::<_, Cardinality, OffsetLen>::new();
-
-    for i in 0..n {
-        nt.push(i);
-    }
-
-    let length_reference = nt.walk(All).expect("Some(_)").depth();
-
-    for i in 0..n {
-        assert_eq!(length_reference, nt.walk(Nth(i)).expect("Some(_)").depth())
-    }
-}
-
-#[test]
-fn persist() {
-    let n: u64 = 1024;
-
-    let mut stack = NStack::<_, Cardinality, OffsetLen>::new();
-    for i in 0..n {
-        stack.push(i);
-    }
-
-    let store = StoreRef::new(HostStore::new());
-    let stored = store.store(&stack);
-
-    let restored: &<NStack<u64, Cardinality, OffsetLen> as Archive>::Archived =
-        store.get::<NStack<_, Cardinality, OffsetLen>>(stored.ident());
-    let restored: NStack<u64, Cardinality, OffsetLen> =
-        restored.deserialize(&mut store.clone()).unwrap();
-
-    // empty original
-    for i in 0..n {
-        assert_eq!(stack.pop().unwrap(), n - i - 1);
-    }
-
-    // check restored copy
-    for i in 0..n {
-        assert_eq!(restored.walk(Nth(i)).unwrap().leaf(), i);
-    }
 }
